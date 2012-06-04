7bed8c5d5931a1930544a40648f348b8c5afa626
ThomasDeneuville <thomas.deneuville@gmail.com>
2012-06-05 00:11:14 +0200

    package like

---
+++ serverSSL/simpleSSLHandler.py
@@ -0,0 +1,42 @@
+"""
+Usage : python simpleSSLHandler.py <PEM file> [port number the server will listen]
+This is the HTTPS server which will receive the connections from the client, and dispatch them to the DropboxRequestHandler
+We need a PEM certificate file that is either signed by an authority in dropbox's trusted_cert.crt, or that is directly in the trusted_cert.crt.
+"""
+import ssl
+import BaseHTTPServer
+
+from WTMH import DropboxRequestHandler
+
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(self):
+        """The do_GET will universally handle every request (HEAD, POST, PUT, DELETE)."""
+        print("%%%%%Hey, I got a message from "+str(self.client_address))
+
+        #This is where the message from the client is handled, and gives a reply to send back to the client.
+        print("%%%%%"+DropboxRequestHandler(self))
+##        self.wfile.write(open("httpResponse").read()) #Sample httpResponse
+        self.send_error(404,"CONNARDS")
+        print("=>Request served !\n")
+    
+    do_HEAD   = do_GET
+    do_POST   = do_GET
+    do_PUT    = do_GET
+    do_DELETE = do_GET
+
+if __name__ == "__main__":
+    import os
+    import sys
+    
+    if len(sys.argv) < 2 or len(sys.argv) > 3 or not os.path.exists(sys.argv[1]):
+        sys.exit('Usage: %s <pem> [port number=4443]' % os.path.basename(__file__))
+
+    if len(sys.argv) == 2:
+        host=('localhost', 4443)
+    else:
+        host=('localhost', int(sys.argv[2]))
+    certificate=sys.argv[1]
+    httpd = BaseHTTPServer.HTTPServer(host, RequestHandler)
+    print("Starting the server on "+str(host))
+    httpd.socket = ssl.wrap_socket(httpd.socket, certfile=certificate, server_side=True)
+    httpd.serve_forever()
