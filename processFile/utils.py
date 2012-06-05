7ff60734f371888a9ecebc336603bd90b361021c
ThomasDeneuville <thomas.deneuville@gmail.com>
2012-06-05 19:46:42 +0200

    with (for file read)

--- processFile/utils.py
+++ processFile/utils.py
@@ -8,17 +8,18 @@ from string import maketrans
 # Be careful : no existing file control... 
 # Could be easily implemented but no need to have a fat code
 
+#It's for text (and not for bin
+
 def getContent(fname):
 	#Read content
-	f = open(fname, 'r')
-	content = f.read()
-	f.close()
+	with f = open(fname, 'r')
+		content = f.read()
 	return content
 
 def setContent(fname, content):
-	f = open(fname, 'w')
-	f.write(content)
-	f.close
+	with f = open(fname, 'w')
+		f.write(content)
+
 
 def goodFileName(fname, name = ""):
 	s = fname + "-" + name
