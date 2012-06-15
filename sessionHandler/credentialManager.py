eb8084952c5b08803d63138241e0801fa721b6c1
gruberg <guillaume.gruber@laposte.net>
2012-06-15 13:22:05 +0200

    handler qui marche à peu près (je sais pas renvoyer un fichier au client

--- sessionHandler/credentialManager.py
+++ sessionHandler/credentialManager.py
@@ -6,11 +6,16 @@ import dropbox_proxy.client_proxy as client_proxy, dropbox_proxy.rest_proxy as r
 
 import processFile.process as process
 
+backup_key = 'jebz0u950mwt6xr'
+backup_secret = 'nyj544pf4b1bnhc'
+backup_token = '96uys2q599w0e6r|f5iexbbbadz2cbi'
+
+
+
 def ClientHandler(url,clientHttpHeader, readFile) :
 	"""handler handling requests from a client"""
 	
 	path = str(url[2])
-
 
 	"""identifying the client"""
 	oauthParsed =parser.parseHeaders(clientHttpHeader)
@@ -18,6 +23,10 @@ def ClientHandler(url,clientHttpHeader, readFile) :
 	consumer_key = oauthParsed['Authorization']['oauth_consumer_key'][1:-1]
 	info=ClientConsumerInfo(consumer_key,"id.conf")
 	consumer_secret=info.split()[1]
+
+	"""creating the db objects for the backup"""
+	backup_sess = session_proxy.DropboxSession(backup_key, backup_secret, 'app_folder', locale=None)
+	backup_sess.set_token(*backup_token.split('|'))
 
 	"""creating a DropboxSession object for the client"""
 	sess = session_proxy.DropboxSession(consumer_key, consumer_secret, 'app_folder', locale=None)
@@ -47,9 +56,8 @@ def ClientHandler(url,clientHttpHeader, readFile) :
 		
 		if path.split("/")[3] == "access_token" : 
 			"""step 2 of authentication"""
-			
-			print "STEP 2"
 
+			print "step 2"
 			#info=ClientConsumerInfo(consumer_key,"tokentemp.conf")
 			#request_token_str="oauth_token_secret="+info.split()[2].split("|")[1]+"&oauth_token="+info.split()[2].split("|")[0]
 			#request_token=oauth.OAuthToken.from_string(request_token_str)
@@ -75,15 +83,11 @@ def ClientHandler(url,clientHttpHeader, readFile) :
 		"""the client has done a put_file"""
 		
 		body_length=oauthParsed["Content-Length"]
-		print body_length
-
 
 		"""we first store the file"""
 		"""we need to know where"""
 		temp_path=str(consumer_key)+"/"+path[13:]
 		to_path=path[13:]
-
-
 
 		"""creating folder if it does not exist"""
 		try :
@@ -91,7 +95,6 @@ def ClientHandler(url,clientHttpHeader, readFile) :
 
 		except OSError:
 			pass
-
 
 		"""write readFile in a file"""
 		with open(temp_path,"wb") as f :
@@ -102,11 +105,19 @@ def ClientHandler(url,clientHttpHeader, readFile) :
 
 		"""encryption of the file"""
 		process.putFile(temp_path) 
+		
 
 		"""sending the encrypted file to dropbox"""
 		with open(temp_path, "rb") as from_file :
 			dbclient=client_proxy.DropboxClient(sess)
 			response=dbclient.put_file(to_path,from_file)
+
+		with open(temp_path, "rb") as from_file :
+			"""send to the backup dropbox account"""
+			backup_to_path='sandbox/'+temp_path
+
+			backup_dbclient=client_proxy.DropboxClient(backup_sess)
+			backup_dbclient.put_file(backup_to_path,from_file)
 
 		"""destroying the temp file"""
 		try :
@@ -118,9 +129,30 @@ def ClientHandler(url,clientHttpHeader, readFile) :
 		return response
 
 
+	if path.split("/")[2] == "files" :
+		"""the client has done a get_file"""
 
+		"""first get the file from dropbox"""
+		from_path=path[9:]
 
+		dbclient=client_proxy.DropboxClient(sess)
+		dbresponse=dbclient.get_file(from_path)
+		
+		"""write the file locally"""
+		local_path=str(consumer_key)+"/"+from_path
 
+		try :
+			os.makedirs(os.path.split(local_path)[0])
+		except OSError:
+			pass
+
+		with open(local_path,"wb") as f :
+			f.write(dbresponse.read())
+
+		"""then check integrity and unencrytion"""
+		process.getFile(local_path)
+
+		"""finally send it to the client"""
 
 
 
@@ -163,6 +195,7 @@ def write_creds(consumer_key, token, TOKEN_FILE):
 
 
 if __name__ == "__main__" :
+	"""samples de requetes pour les tests"""
 	req1 = " method: POST ; host: ('api.dropbox.com', 443) ; path: /1/oauth/request_token ; proto: HTTP/1.1 ; len(body): 168\n  Content-Length: 168\n  Accept-Encoding: identity\n  User-Agent: OfficialDropboxPythonSDK/1.4\n Host: api.dropbox.com\n  Content-type: application/x-www-form-urlencoded\n  Authorization: OAuth realm=\"\", oauth_nonce=\"28356426\", oauth_timestamp=\"1337941763\", oauth_consumer_key=\"6y443oxnwt7q32y\", oauth_signature_method=\"PLAINTEXT\", oauth_version=\"1.0\", oauth_signature=\"7315tog2zjsch4l%26\"\n\nBody : oauth_nonce=28356426&oauth_timestamp=1337941763&oauth_consumer_key=6y443oxnwt7q32y&oauth_signature_method=PLAINTEXT&oauth_version=1.0&oauth_signature=7315tog2zjsch4l%26\n"
 	
 
